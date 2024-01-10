ac2dd51797d99aedf937cb4a7cee8bb61d264126
m.shamsiev <m.shamsiev@yt.uz>
2024-01-10 12:37:59 +0500

    List page APIs connect

--- src/pages/List/components/TableList/TableList.tsx
+++ src/pages/List/components/TableList/TableList.tsx
@@ -1,16 +1,48 @@
-import { FC, useCallback } from "react";
+import { FC, useCallback, useEffect, useMemo } from "react";
 import { useTranslation } from "react-i18next";
-import classes from "./TableList.module.scss";
 import { ColumnsType, Table } from "components/shared";
 import { Badge } from "antd";
+import {
+  useInfoGetAllMessageStatusByNumber,
+  useInfoGetAllMessageStatusByNumberOperator,
+} from "hooks";
+import { useSearchParams } from "react-router-dom";
+import { MessageStatusDto } from "types";
 
-type RecordDto = any;
 type ExpandedDataDto = any;
 
 const TableList: FC = () => {
   const { t } = useTranslation();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const number = searchParams.get("phone")!;
+  const operator = searchParams.get("operator")!;
 
-  const columns: ColumnsType<RecordDto> = [
+  useEffect(() => {
+    if (!number) {
+      searchParams.delete("operator");
+      setSearchParams(searchParams);
+    }
+  }, [number, searchParams, setSearchParams]);
+
+  const messageQuery = useInfoGetAllMessageStatusByNumber({
+    number,
+  });
+
+  const messageOperatorQuery = useInfoGetAllMessageStatusByNumberOperator(
+    {
+      number,
+      operator,
+    },
+    { enabled: !!operator }
+  );
+  const expandedDataMessage = useMemo(
+    () => messageOperatorQuery.data?.data?.data!,
+    [messageOperatorQuery.data]
+  );
+
+  const dataSource = messageQuery.data?.data?.data!;
+
+  const columns: ColumnsType<MessageStatusDto> = [
     {
       title: "№",
       render: (_, __, idx) => idx + 1,
@@ -19,45 +51,70 @@ const TableList: FC = () => {
     },
     {
       title: t("Телефон"),
-      dataIndex: "phone",
+      dataIndex: "phoneNumber",
       align: "center",
+      width: "30%",
     },
     {
       title: t("Юборилган вақт"),
-      dataIndex: "sendTime",
+      dataIndex: "createdDate",
       align: "center",
+      width: "30%",
     },
     {
       title: t("Ҳолати"),
-      dataIndex: "state",
+      dataIndex: "status",
       align: "center",
-      render: (value) => <Badge status="success" text={value} />,
+      width: "30%",
+      render: (value) => {
+        return value ? <Badge status="success" text={value} /> : "-";
+      },
     },
   ];
 
   const expandedRowRender = useCallback(() => {
     const columns: ColumnsType<ExpandedDataDto> = [
       { title: "№", align: "center", width: 80, render: (_, __, idx) => idx + 1 },
-      { title: t("Оператор"), dataIndex: "name" },
-      { title: t("Қачон кетди"), dataIndex: "" },
-      { title: t("Жавоб ҳолати"), dataIndex: "" },
+      { title: t("Оператор"), dataIndex: "operator", align: "center" },
+      { title: t("Қачон кетди"), dataIndex: "createdDate", align: "center" },
+      {
+        title: t("Жавоб ҳолати"),
+        dataIndex: "status",
+        align: "center",
+        render: (value) => (value ? <Badge status="processing" text={value} /> : "-"),
+      },
     ];
 
-    return <Table columns={columns} dataSource={[{}, {}]} pagination={false} />;
-  }, []);
+    return (
+      <Table
+        columns={columns}
+        dataSource={expandedDataMessage}
+        loading={messageOperatorQuery.isFetching}
+        pagination={false}
+      />
+    );
+  }, [expandedDataMessage, messageOperatorQuery.isFetching, t]);
 
   return (
     <div>
       <Table
         rowKey={({ id }) => id!}
         columns={columns}
-        dataSource={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]}
-        loading={false}
+        dataSource={dataSource}
+        loading={messageQuery.isLoading}
         expandable={{
           expandedRowRender,
           expandIconColumnIndex: 5,
           columnTitle: t("Батафсил кўриш"),
           columnWidth: 150,
+          onExpand(expanded, { operator }) {
+            if (operator) {
+              searchParams.set("operator", operator);
+            } else {
+              searchParams.delete("operator");
+            }
+            setSearchParams(searchParams);
+          },
         }}
         pagination={false}
       />
